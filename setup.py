6b5578deb0f9a01dfe96784faef46df3c36888d8
sebastian Cabezon <secabezon21@gmail.com>
2025-10-10 20:23:17 -0300

    Estructura cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='project Mlops Tec 2025',
+    author='Grupo_5_tec',
+    license='MIT',
+)
